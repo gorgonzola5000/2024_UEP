cbc34bf120cc5a91ea327e8134b195b6a3a0399c
kamilvmichalak <kamilvmichalak@gmail.com>
2024-04-25 16:11:02 +0200

    MachineFactory bez ifa

--- battle-model/src/main/java/pl/psi/creatures/MachineFactory.java
+++ battle-model/src/main/java/pl/psi/creatures/MachineFactory.java
@@ -6,8 +6,6 @@ public class MachineFactory
 {   private static final String EXCEPTION_MESSAGE = "We support tiers from 8 to 10";
     public Creature create( final boolean aIsUpgraded, final int aTier, final int aAmount )
     {
-        if( !aIsUpgraded )
-        {
             switch( aTier )
             {
                 case 8:
@@ -26,27 +24,6 @@ public class MachineFactory
                 default:
                     throw new IllegalArgumentException( EXCEPTION_MESSAGE );
             }
-        }
-        else
-        {
-            switch( aTier )
-            {
-                case 8:
-                    return new Creature.Builder().statistic( CreatureStatistic.FIRST_AID_TENT )
-                            .amount( aAmount )
-                            .build();
-                case 9:
-                    return new Creature.Builder().statistic( CreatureStatistic.BALLISTA )
-                            .amount( aAmount )
-                            .build();
-                case 10:
-                    return new Creature.Builder().statistic( CreatureStatistic.CATAPULT )
-                            .amount( aAmount )
-                            .build();
-                default:
-                    throw new IllegalArgumentException( EXCEPTION_MESSAGE );
-            }
-        }
     }
 
     public void healHPCreature(Creature creature) {
