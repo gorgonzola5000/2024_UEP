f77ae3c17bc4180c1a0f3082c550eccd06256ef5
Stanislaw <stanislawstachowski112@gmail.com>
2024-04-25 18:27:26 +0200

    Creature now applies damage to itself

--- battle-model/src/main/java/pl/psi/creatures/Creature.java
+++ battle-model/src/main/java/pl/psi/creatures/Creature.java
@@ -63,18 +63,19 @@ public class Creature implements PropertyChangeListener {
     }
 
     private void applyDamage(DamageValueObject aDamageValueObject) {
-//        int hpToSubstract = aDamageValueObject.getDamageAmount() % this.getMaxHp();
-//        int amountToSubstract = Math.round(aDamageValueObject.getDamageAmount() / this.getMaxHp());
-//
-//        int hp = this.getCurrentHp() - hpToSubstract;
-//        if (hp <= 0) {
-//            this.setCurrentHp(this.getMaxHp() - hp);
-//            this.setAmount(this.getAmount() - 1);
-//        }
-//        else{
-//            this.setCurrentHp(hp);
-//        }
-//        this.setAmount(this.getAmount() - amountToSubstract);
+        int hpToSubstract = aDamageValueObject.getDamageAmount() % this.getMaxHp();
+        int amountToSubstract = Math.round(aDamageValueObject.getDamageAmount() / this.getMaxHp());
+
+        int hp = this.getCurrentHp() - hpToSubstract;
+        if (hp <= 0) {
+            this.setCurrentHp(this.getMaxHp() - hp);
+            this.setAmount(this.getAmount() - 1);
+        }
+        else{
+            this.setCurrentHp(hp);
+        }
+        this.setAmount(this.getAmount() - amountToSubstract);
+        //TODO move this to DamageApplierObject
     }
 
     public int getMaxHp() {
